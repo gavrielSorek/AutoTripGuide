c08a4c2adc86d77d4fd8863f1d4d7fda4518cd83
Gavriel Sorek <gavrielsorek@gmail.com>
2021-12-05 14:11:32 +0200

    add the option to get position by name with different languages

--- Web Tool/server/WikiServices/positionByNameWiki.js
+++ Web Tool/server/WikiServices/positionByNameWiki.js
@@ -1,15 +1,15 @@
 module.exports = {getPositionByName};
 const wiki = require('wikijs').default;
 //the function return postion in dd coordinate by name, this function teturn promise
-async function getPositionByName(title) {
+async function getPositionByName(title, language) {
 
-  return wiki({ apiUrl: 'https://es.wikipedia.org/w/api.php' })
+  return wiki({ apiUrl: "https://"+ language +".wikipedia.org/w/api.php"})
     .page(title)
     .then(page => page.coordinates())
 }
-//example function how to use getPositionByName
+// example function how to use getPositionByName
 // function example() {
-//   var ex = getPositionByName('masada')
+//   var ex = getPositionByName('masada', 'en')
 //   ex.then((position)=>{console.log("lat: " + position.lat + " lng: " + position.lon)}).catch(()=>{console.log("error cant find this position")});
 // }
 // example()
