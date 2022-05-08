ccfeadff4d4f491e0df8a28d8acd324af45e863f
Gavriel Sorek <gavrielsorek@gmail.com>
2022-05-08 18:50:53 +0300

    dialog box supports loading

--- mobile_app_auto_trip_guide/lib/Map/dialog_box.dart
+++ mobile_app_auto_trip_guide/lib/Map/dialog_box.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -17,20 +18,38 @@ class CustomDialogBox extends StatefulWidget {
 
   CustomDialogBox(
       {required Key key,
-      required this.title,
-      required this.descriptions,
-      required this.leftButtonText,
-      required this.rightButtonText,
-      required this.img,
-      this.onPressLeft,
-      this.onPressRight})
+        required this.title,
+        required this.descriptions,
+        required this.leftButtonText,
+        required this.rightButtonText,
+        required this.img,
+        this.onPressLeft,
+        this.onPressRight})
       : super(key: key);
 
+  // progress between 0 - 1
+  setProgress(progress) {
+    _customDialogBoxState?.setProgress(progress);
+  }
+
+  _CustomDialogBoxState? _customDialogBoxState;
+
   @override
-  _CustomDialogBoxState createState() => _CustomDialogBoxState();
+  _CustomDialogBoxState createState() {
+    _customDialogBoxState = _CustomDialogBoxState();
+    return _customDialogBoxState!;
+  }
 }
 
 class _CustomDialogBoxState extends State<CustomDialogBox> {
+  double progress = 0.11; // the smallest progress that possible
+  // progress between 0 - 1
+  setProgress(progress) {
+    setState(() {
+      // progress in reality can be [0.11, 1]
+      this.progress = min(1,max(progress, 0.11));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -48,66 +67,98 @@ class _CustomDialogBoxState extends State<CustomDialogBox> {
     return Stack(
       children: <Widget>[
         Container(
-          padding: const EdgeInsets.only(
-              left: Constants.padding,
-              top: Constants.avatarRadius + Constants.padding,
-              right: Constants.padding,
-              bottom: Constants.padding),
-          margin: EdgeInsets.only(top: Constants.avatarRadius),
-          decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(Constants.padding),
-              boxShadow: const [
-                BoxShadow(
-                    color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
-              ]),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                widget.title,
-                style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Text(
-                widget.descriptions,
-                style: const TextStyle(fontSize: 14),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 22,
-              ),
+            padding: const EdgeInsets.only(
+                left: Constants.padding,
+                top: Constants.avatarRadius + Constants.padding,
+                right: Constants.padding,
+                bottom: Constants.padding),
+            margin: EdgeInsets.only(top: Constants.avatarRadius),
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(Constants.padding),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(0, 10),
+                      blurRadius: 10),
+                ]),
+            child: Column(children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  FlatButton(
-                      onPressed: () {
-                        if (widget.onPressLeft != null) {
-                          widget.onPressLeft();
-                        }
-                      },
-                      child: Text(
-                        widget.leftButtonText,
-                        style: TextStyle(fontSize: 18),
-                      )),
-                  FlatButton(
-                      onPressed: () {
-                        if (widget.onPressRight != null) {
-                          widget.onPressRight();
-                        }
-                      },
-                      child: Text(
-                        widget.rightButtonText,
-                        style: TextStyle(fontSize: 18),
-                      )),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      left: Constants.padding,
+                      right: Constants.padding,
+                      top: 5,),
+                    color: Colors.black,
+                    width: progress * MediaQuery.of(context).size.width - 2 * Constants.padding,
+                  ),
+                  // Container(
+                  //   padding: const EdgeInsets.only(
+                  //       left: Constants.padding,
+                  //       top: Constants.avatarRadius + Constants.padding,
+                  //       right: Constants.padding,
+                  //       bottom: Constants.padding),
+                  //   color: Colors.green,
+                  //
+                  //
+                  // )
+
                 ],
+              ),
+              Container(
+                padding: const EdgeInsets.only(
+                    top: 5),
+                color: Colors.white,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      widget.title,
+                      style: const TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      widget.descriptions,
+                      style: const TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FlatButton(
+                            onPressed: () {
+                              if (widget.onPressLeft != null) {
+                                widget.onPressLeft();
+                              }
+                            },
+                            child: Text(
+                              widget.leftButtonText,
+                              style: TextStyle(fontSize: 18),
+                            )),
+                        FlatButton(
+                            onPressed: () {
+                              if (widget.onPressRight != null) {
+                                widget.onPressRight();
+                              }
+                            },
+                            child: Text(
+                              widget.rightButtonText,
+                              style: TextStyle(fontSize: 18),
+                            )),
+                      ],
+                    )
+                  ],
+                ),
               )
-            ],
-          ),
-        ),
+            ])),
         Positioned(
           left: Constants.padding,
           right: Constants.padding,
@@ -116,7 +167,7 @@ class _CustomDialogBoxState extends State<CustomDialogBox> {
               radius: Constants.avatarRadius,
               child: ClipRRect(
                 borderRadius:
-                    BorderRadius.all(Radius.circular(Constants.avatarRadius)),
+                BorderRadius.all(Radius.circular(Constants.avatarRadius)),
                 child: widget.img,
               )),
         ),
