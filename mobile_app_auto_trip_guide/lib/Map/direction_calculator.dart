2f9c6edf689898570727dfda473fe6f7fc2b095d
Sapir David <sapirdavid5220@gmail.com>
2022-05-27 13:57:04 +0300

    Add direction calculator

---
+++ mobile_app_auto_trip_guide/lib/Map/direction_calculator.dart
@@ -0,0 +1,24 @@
+import 'dart:math' as Math;
+
+class DirectionCalculator {
+  static Map<int, String> Directions = {
+    0: 'North',
+    4: 'South',
+    6: 'East',
+    2: 'West',
+    7: 'Northeast',
+    1: 'Northwest',
+    5: 'Southeast',
+    3: 'Southwest',
+   -1: 'Undefined'
+  };
+
+  static String getDirection(double lat1, double lng1, double lat2, double lng2) {
+    double angle = Math.atan2(lat2 - lat1, lng2 - lng1);
+    angle += Math.pi;
+    angle /= Math.pi / 4;
+    int halfQuarter = angle.toInt();
+    halfQuarter %= 8;
+    return Directions[halfQuarter] ?? 'Undefined';
+  }
+}
