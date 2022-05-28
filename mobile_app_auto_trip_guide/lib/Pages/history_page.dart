99e82a796a4ff5b962bdf073c8c791b8623b2393
Gavriel Sorek <gavrielsorek@gmail.com>
2022-05-28 21:06:00 +0300

    Add toolbar to history page

--- mobile_app_auto_trip_guide/lib/Pages/history_page.dart
+++ mobile_app_auto_trip_guide/lib/Pages/history_page.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import '../Map/events.dart';
 import '../Map/globals.dart';
 import '../Map/types.dart';
-
+import '../UsefulWidgets/toolbar.dart';
 
 class HistoryPage extends StatefulWidget {
   HistoryPage({Key? key}) : super(key: key);
+
   @override
   _HistoryPageState createState() => _HistoryPageState();
 }
@@ -18,29 +19,33 @@ class _HistoryPageState extends State<HistoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: buildAppBar(context),
-        body: ListView.separated(
-          itemBuilder: (BuildContext, index){
-            int position = visitedPoisListLength - index - 1;
-            return ListTile(
-              leading: CircleAvatar(backgroundColor: Colors.transparent, child: ClipRRect(
-                                borderRadius:
-                                const BorderRadius.all(Radius.circular(15)),
-                                child: Image.network(visitedPoisList[position].pic ?? ""),
-                              )),
-              title: Text(visitedPoisList[position].poiName ?? "https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2019/02/07/1127/Andaz-Costa-Rica-P834-Aerial-Culebra-Bay-View.jpg/Andaz-Costa-Rica-P834-Aerial-Culebra-Bay-View.16x9.jpg"),
-              subtitle: Text(visitedPoisList[position].time),
-            );
-          },
-          separatorBuilder: (BuildContext,index)
-          {
-            return const Divider(height: 1);
-          },
-          itemCount: visitedPoisList.length,
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(5),
-          scrollDirection: Axis.vertical,
-        )
-    );
+        body: Column(mainAxisSize: MainAxisSize.min, children: [
+          Expanded(
+              child: ListView.separated(
+            itemBuilder: (BuildContext, index) {
+              int position = visitedPoisListLength - index - 1;
+              return ListTile(
+                leading: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      child: Image.network(visitedPoisList[position].pic ?? ""),
+                    )),
+                title: Text(visitedPoisList[position].poiName ??
+                    "https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2019/02/07/1127/Andaz-Costa-Rica-P834-Aerial-Culebra-Bay-View.jpg/Andaz-Costa-Rica-P834-Aerial-Culebra-Bay-View.16x9.jpg"),
+                subtitle: Text(visitedPoisList[position].time),
+              );
+            },
+            separatorBuilder: (BuildContext, index) {
+              return const Divider(height: 1);
+            },
+            itemCount: visitedPoisList.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(5),
+            scrollDirection: Axis.vertical,
+          )),
+          const Toolbar()
+        ]));
   }
 
   AppBar buildAppBar(BuildContext context) {
