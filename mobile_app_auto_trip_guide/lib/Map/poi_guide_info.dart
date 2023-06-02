8a78f3ea99685ee781ee792abc503dd996bb8d75
Gavriel <gavrielsorek@gmail.com>
2023-06-02 18:57:29 +0300

    update poiGuideInfo Widget

--- mobile_app_auto_trip_guide/lib/Map/poi_guide_info.dart
+++ mobile_app_auto_trip_guide/lib/Map/poi_guide_info.dart
@@ -1,8 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:final_project/Map/types.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../General Wigets/stretching_widget.dart';
+import 'globals.dart';
 
 class PoiGuide extends StatefulWidget {
   @override
@@ -32,47 +35,98 @@ class _PoiGuideState extends State<PoiGuide> {
   }
 
   get expendedPoiInfo {
-    return Container(
-        decoration: StretchingWidget.boxDecoration,
-        child: Column(children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.3 ,
-            child: Stack(
-              children: [
-                PoiGuideImageWidget(
-                  imagePath: 'https://www.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg',
-                  boxDecoration: StretchingWidget.boxDecoration,
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: GuideControls(),
-                ),
-                // Large square picture
-              ],
-            ),
-          ),
-          SingleChildScrollView(
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-                      'Phasellus sed consequat libero. Etiam tempus, ligula non cursus '
-                      'fringilla, augue libero ornare sapien, in commodo mauris diam '
-                      'ac justo. Morbi maximus, mauris at efficitur eleifend, purus '
-                      'mauris vulputate nulla, eu auctor leo leo a justo. Curabitur '
-                      'molestie purus id dolor suscipit, nec rhoncus lacus ultrices. '
-                      'In finibus quam ac ante semper, et tincidunt sapien faucibus. '
-                      'Nam convallis, est at facilisis laoreet, enim dui placerat '
-                      'enim, sed faucibus lectus mauris non elit. In sit amet dapibus '
-                      'purus. Donec dignissim justo eu tellus accumsan convallis. '
-                      'In consequat purus in nisi efficitur lobortis. Nam nec tempus '
-                      'risus, at laoreet lacus. Quisque id scelerisque lectus.',
-                ),
+    return Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.3,
+          child: Stack(
+            children: [
+              PoiGuideImageWidget(
+                imagePath:
+                'https://www.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg',
+                boxDecoration: StretchingWidget.boxDecoration,
               ),
-            ),
+              Align(
+                alignment: Alignment.center,
+                child: GuideControls(),
+              ),
+              // Large square picture
+            ],
           ),
-        ]));
+        ),
+        Expanded(
+          child: Container(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 16,
+                ),
+                child: Center(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 0, right: 0),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              child: Container(
+                                child: Text(
+                                  'title',
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontFamily: 'Inter',
+                                      fontSize: 22,
+                                      letterSpacing: 0.3499999940395355,
+                                      fontWeight: FontWeight.normal,
+                                      height: 1.2727272727272727),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                        'Phasellus sed consequat libero. Etiam tempus, ligula non cursus '
+                        'fringilla, augue libero ornare sapien, in commodo mauris diam '
+                        'ac justo. Morbi maximus, mauris at efficitur eleifend, purus '
+                        'mauris vulputate nulla, eu auctor leo leo a justo. Curabitur '
+                        'molestie purus id dolor suscipit, nec rhoncus lacus ultrices. '
+                        'In finibus quam ac ante semper, et tincidunt sapien faucibus. '
+                        'Nam convallis, est at facilisis laoreet, enim dui placerat '
+                        'enim, sed faucibus lectus mauris non elit. In sit amet dapibus '
+                        'ac justo. Morbi maximus, mauris at efficitur eleifend, purus '
+                        'mauris vulputate nulla, eu auctor leo leo a justo. Curabitur '
+                        'molestie purus id dolor suscipit, nec rhoncus lacus ultrices. '
+                        'In finibus quam ac ante semper, et tincidunt sapien faucibus. '
+                        'Nam convallis, est at facilisis laoreet, enim dui placerat '
+                        'enim, sed faucibus lectus mauris non elit. In sit amet dapibus '
+                        'purus. Donec dignissim justo eu tellus accumsan convallis. '
+                        'In consequat purus in nisi efficitur lobortis. Nam nec tempus '
+                        'risus, at laoreet lacus. Quisque id scelerisque lectus.',
+                        style: TextStyle(
+                            color: Color(0xff6C6F70),
+                            fontFamily: 'Inter',
+                            fontSize: 16,
+                            letterSpacing: 0,
+                            fontWeight: FontWeight.normal,
+                            height: 1.5),
+                        textAlign: TextAlign.left,
+                      )
+                    ]),
+                  ),
+                ),
+                //color: backgroundColor,
+              )),
+        ),
+        BottomBarWidget(
+          poi: Poi(id: 'a', latitude: 38, longitude: 38, Categories: []),
+        )
+      ],
+    );
   }
 }
 
@@ -81,9 +135,7 @@ class PoiGuideImageWidget extends StatelessWidget {
   final BoxDecoration boxDecoration;
 
   const PoiGuideImageWidget(
-      {Key? key,
-      required this.imagePath,
-      required this.boxDecoration})
+      {Key? key, required this.imagePath, required this.boxDecoration})
       : super(key: key);
 
   @override
@@ -115,7 +167,6 @@ class _GuideControlsState extends State<GuideControls> {
   Icon pauseIcon = Icon(Icons.pause);
   bool isPlaying = false;
 
-
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -123,7 +174,8 @@ class _GuideControlsState extends State<GuideControls> {
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.5), // Black color with 50% opacity
+            color: Colors.black.withOpacity(0.5),
+            // Black color with 50% opacity
             shape: BoxShape.circle, // Assuming you want a circular background
           ),
           child: IconButton(
@@ -146,7 +198,8 @@ class _GuideControlsState extends State<GuideControls> {
           width: 60,
           height: 60,
           decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.5), // Black color with 50% opacity
+            color: Colors.black.withOpacity(0.5),
+            // Black color with 50% opacity
             shape: BoxShape.circle, // Assuming you want a circular background
           ),
           child: IconButton(
@@ -162,16 +215,16 @@ class _GuideControlsState extends State<GuideControls> {
         ),
         Container(
           decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.5), // Black color with 50% opacity
+            color: Colors.black.withOpacity(0.5),
+            // Black color with 50% opacity
             shape: BoxShape.circle, // Assuming you want a circular background
           ),
           child: GestureDetector(
-            onTap: (){
+            onTap: () {
               int mm = 6;
             },
             child: IconButton(
-              onPressed: () {
-              },
+              onPressed: () {},
               icon: SvgPicture.asset(
                 'assets/images/double-right-chevron-svgrepo-com.svg',
                 width: 22,
@@ -184,6 +237,117 @@ class _GuideControlsState extends State<GuideControls> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class BottomBarWidget extends StatefulWidget {
+  final Poi poi;
+
+  BottomBarWidget({required this.poi});
+
+  @override
+  _BottomBarWidgetState createState() => _BottomBarWidgetState();
+}
+
+class _BottomBarWidgetState extends State<BottomBarWidget> {
+  double bottomIconSize = 20;
+  int poiPreference = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RawMaterialButton(
+                onPressed: () {
+                  Globals.globalAppLauncher.launchWaze(
+                    widget.poi.latitude,
+                    widget.poi.longitude,
+                  );
+                },
+                elevation: 2.0,
+                fillColor: Colors.blue,
+                child: Icon(
+                  Icons.directions,
+                  size: bottomIconSize,
+                ),
+                shape: CircleBorder(),
+              ),
+              Opacity(
+                opacity: poiPreference == -1 ? 1.0 : 0.5,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    Globals.appEvents.poiNavigationStarted(
+                      widget.poi.poiName ?? '',
+                      widget.poi.Categories,
+                      widget.poi.id,
+                    );
+                    poiPreference = -1;
+                    Globals.globalServerCommunication.insertPoiPreferences(
+                      widget.poi.id,
+                      Globals.globalUserInfoObj,
+                      poiPreference,
+                    );
+                    setState(() {});
+                  },
+                  elevation: 2.0,
+                  fillColor: Colors.red,
+                  child: Icon(
+                    Icons.thumb_down,
+                    size: bottomIconSize,
+                  ),
+                  shape: CircleBorder(),
+                ),
+              ),
+              Opacity(
+                opacity: poiPreference == 1 ? 1.0 : 0.5,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    poiPreference = 1;
+                    Globals.globalServerCommunication.insertPoiPreferences(
+                      widget.poi.id,
+                      Globals.globalUserInfoObj,
+                      poiPreference,
+                    );
+                    setState(() {});
+                  },
+                  elevation: 2.0,
+                  fillColor: Colors.green,
+                  child: Icon(
+                    Icons.thumb_up,
+                    size: bottomIconSize,
+                  ),
+                  shape: CircleBorder(),
+                ),
+              ),
+              RawMaterialButton(
+                onPressed: () {
+                  Globals.appEvents.poiShared(
+                    widget.poi.poiName ?? '',
+                    widget.poi.Categories,
+                    widget.poi.id,
+                  );
+                  Share.share(
+                    widget.poi.shortDesc ?? "",
+                    subject: widget.poi.poiName,
+                  );
+                },
+                elevation: 2.0,
+                fillColor: Colors.blue,
+                child: Icon(
+                  Icons.share,
+                  size: bottomIconSize,
+                ),
+                shape: CircleBorder(),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
