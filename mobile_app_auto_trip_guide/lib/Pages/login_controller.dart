3a748777eb50267cb38690e49d901bf4c6a2c0ac
Gavriel <gavrielsorek@gmail.com>
2023-06-12 22:58:56 +0300

    Add error handling in sign in with google

--- mobile_app_auto_trip_guide/lib/Pages/login_controller.dart
+++ mobile_app_auto_trip_guide/lib/Pages/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -7,7 +8,18 @@ class LoginController extends GetxController {
   bool _isSignedIn = false;
 
   login() async {
-    googleAccount.value = await _googleSignin.signIn();
+    bool success = false;
+
+    while (!success) {
+      try {
+        googleAccount.value = await _googleSignin.signIn();
+        _isSignedIn = await _googleSignin.isSignedIn();
+        success = true;
+      } catch (e) {
+        // Wait for 5 seconds before retrying
+        await Future.delayed(Duration(seconds: 5));
+      }
+    }
     _isSignedIn = await _googleSignin.isSignedIn();
   }
 
