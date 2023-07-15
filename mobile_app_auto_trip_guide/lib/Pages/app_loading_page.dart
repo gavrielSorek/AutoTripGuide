0d191474a2c8164cb374fd4e80b8c6612f30f793
gavrielSorek <72670796+gavrielSorek@users.noreply.github.com>
2023-07-15 11:07:36 +0300

    Added logo to the loading page

--- mobile_app_auto_trip_guide/lib/Pages/app_loading_page.dart
+++ mobile_app_auto_trip_guide/lib/Pages/app_loading_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../Map/globals.dart';
 
@@ -37,17 +37,59 @@ class _AppInitializationPageState extends State<AppInitializationPage> {
 
     Navigator.pop(context);
     Navigator.pushNamed(context, initialRoute);
-  }
+   }
 
+  @override
+  Widget build(BuildContext context) {
+    return LoadingAppWidget();
+  }
+}
+
+class LoadingAppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.blue,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(10, 132, 255, 1),
+        ),
         child: Center(
-          child: SpinKitRotatingCircle(
-            color: Colors.white,
-            size: 50.0,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/logo_white.png',
+                width: 280,
+                height: 280,
+              ),
+              Shimmer.fromColors(
+                baseColor: Colors.white.withOpacity(0.3),  // stronger base color
+                highlightColor: Colors.black38,
+                period: Duration(milliseconds: 2000),  // slower speed
+                child: Image.asset(
+                  'assets/images/logo_white.png',  // your png asset
+                  width: 280,
+                  height: 280,
+                ),
+              ),
+              Positioned(
+                bottom: 30.0,
+                child: Text(
+                  'Discover on the go!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    fontFamily: 'Inter',
+                    fontSize: 19,
+                    letterSpacing: 0.3499999940395355,
+                    fontWeight: FontWeight.normal,
+                    height: 1.4736842105263157,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
